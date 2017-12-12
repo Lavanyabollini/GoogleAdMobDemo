53a3220fa1908b363231904a862874ef9c970ca1
mobinius <narendra.v@mobinius.com>
2017-12-12 18:32:09 +0530

    adding new file for google add integration in app

---
+++ GoogleAdMobDemo/NewTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  NewTableViewCell.swift
+//  GoogleAdMobDemo
+//
+//  Created by mobinius on 12/12/17.
+//  Copyright © 2017 mobinius. All rights reserved.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+    @IBOutlet  var postImageView: UIImageView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GoogleAdMobDemo/ViewController.swift
+++ GoogleAdMobDemo/ViewController.swift
@@ -7,19 +7,95 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController, GADBannerViewDelegate{
+    
+   var imagesArray=[UIImage]()
+ var bannerView: GADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagesArray = [ UIImage(named: "Image1.png")!,
+                            UIImage(named: "Image2.png")!,
+                            UIImage(named: "Image3.png")!,
+                            UIImage(named: "Image4.png")!
+                            ]
+        bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        bannerView?.adUnitID = "ca-app-pub-3827126407335086/6839878394"
+        bannerView?.rootViewController = self
+         bannerView?.load(GADRequest())
+         bannerView?.delegate = self
+        
     }
+    
+   
+    
+ 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //MARK:-GADBannerView delegate methods
+    /// Tells the delegate an ad request loaded an ad.
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        
+        print("Banner loaded successfully")
+        // Reposition the banner ad to create a slide down effect
+        let translateTransform = CGAffineTransform(translationX: 0, y: -bannerView.bounds.size.height)
+        bannerView.transform = translateTransform
+        
+        UIView.animate(withDuration: 0.5) {
+            bannerView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    /// Tells the delegate an ad request failed.
+    func adView(_ bannerView: GADBannerView,
+                didFailToReceiveAdWithError error: GADRequestError) {
+        print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+    
+    /// Tells the delegate that a full-screen view will be presented in response
+    /// to the user clicking on an ad.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+        print("adViewWillPresentScreen")
+    }
+    
+    /// Tells the delegate that the full-screen view will be dismissed.
+    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+        print("adViewWillDismissScreen")
+    }
+    
+    /// Tells the delegate that the full-screen view has been dismissed.
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+        print("adViewDidDismissScreen")
+    }
+    
+    /// Tells the delegate that a user click will open another app (such as
+    /// the App Store), backgrounding the current app.
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+        print("adViewWillLeaveApplication")
+    }
+    
+    //MARK:- UITableView Datasource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imagesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewTableViewCell
+        cell.postImageView.image=imagesArray[indexPath.row]
+        return cell
+    }
+    //MARK:- UITableView Delegate
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return bannerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return bannerView.frame.height
+    }
 }
 
